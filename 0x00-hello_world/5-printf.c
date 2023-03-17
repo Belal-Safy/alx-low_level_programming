f39d9f1eab09c60bb2b7894f09bb95d27b6a0004
Belal-Safy <belal.aglan2@gmail.com>
2023-03-17 04:29:49 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,8 @@
 * Return: Always 0 (Success)
 */
 
-int main(viod) {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
