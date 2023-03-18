3262db5c9705a839d69010b779af4f86a5966c9e
Belal-Safy <belal.aglan2@gmail.com>
2023-03-17 18:00:52 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,21 +5,20 @@
 * Return: Always 0 (Success)
 */
 
-int main(void)
-{
-int n, m;
-for (n = '0'; n <= '8'; n++)
-{
-for (m = '1'; m <= '9'; m++)
-{
+int main(void) {
+int n, m, itr;
+itr = 0;
+
+for (n = '0'; n <= '8'; n++) {
+for (m = '1' + itr; m <= '9'; m++) {
 putchar(n);
 putchar(m);
-if (n != '8' || m !='9')
-{
+if (n != '8' || m != '9') {
 putchar(',');
 putchar(' ');
 }
 }
+itr ++;
 }
 putchar('\n');
 
