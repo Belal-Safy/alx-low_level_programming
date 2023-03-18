345c8d8c94dc2fe88986f344f79a28df0a0b2e8a
Belal-Safy <belal.aglan2@gmail.com>
2023-03-17 17:20:10 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,11 +24,11 @@ printf("greater than 5\n");
 }
 else if (last == 0)
 {
-printf("0");
+printf("0\n");
 }
 else if (last < 6)
 {
-printf("less than 6 and not 0");
+printf("less than 6 and not 0\n");
 }
 return (0);
 }
