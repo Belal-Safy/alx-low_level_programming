56051df8a2a5f0a54c3d58ab9ac13fb50270d740
Belal Safy <81881113+Belal-Safy@users.noreply.github.com>
2023-03-23 01:44:19 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
-#include <stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * c: The character to print
+ *
+ * Return: void
+ */
+ 
+void _putchar(char c)
+{
+write(1, &c, 1);   
+}
 
 /**
-* main - Entry point
-* Return: Always 0 (Success)
-*/
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
