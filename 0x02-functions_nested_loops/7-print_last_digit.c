216cdd581e0822b3371743aef368ce4d654fcf5c
Belal Safy <81881113+Belal-Safy@users.noreply.github.com>
2023-03-23 03:30:56 +0200

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @c: character
+ * Return: Always 0.
+ */
+
+int print_last_digit(int c)
+{
+int last = c % 10;
+printf(last);
+return (last);
+}
