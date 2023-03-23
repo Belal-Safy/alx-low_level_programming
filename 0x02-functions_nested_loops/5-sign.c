c55ad0cfc24f5f2e25dd202aeeafed4b0696a299
Belal Safy <81881113+Belal-Safy@users.noreply.github.com>
2023-03-23 02:20:47 +0200

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code
+ * @c: character
+ * Return: Always 0.
+ */
+
+int print_sign(int c)
+{
+if (c > 0)
+return (1);
+else if c == 0)
+return (0);
+else
+return (-1);
+}
