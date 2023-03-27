51c91dcef14b7dcfbfb17a14d241c2c1d364aac1
Belal Safy <81881113+Belal-Safy@users.noreply.github.com>
2023-03-27 23:26:01 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
   * rev_string - Reverses a string
@@ -8,27 +9,31 @@
   */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
-
-	a = s;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
+    int l, i;
+    char *begin_ptr, *end_ptr, ch;
+ 
+    // Get the length of the string
+    l = strlen(s);
+ 
+    // Setting the begin_ptr
+    // to start of string
+    begin_ptr = s;
+   
+      //Setting the end_ptr the end of
+      //the string
+    end_ptr = s + l - 1;
+ 
+    // Swap the char from start and end
+    // index using begin_ptr and end_ptr
+    for (i = 0; i < (l - 1) / 2; i++) {
+ 
+        // swap character
+        ch = *end_ptr;
+        *end_ptr = *begin_ptr;
+        *begin_ptr = ch;
+ 
+        // update pointers positions
+        begin_ptr++;
+        end_ptr--;
+    }
 }
-
