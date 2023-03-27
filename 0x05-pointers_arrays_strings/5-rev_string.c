e7ffa855f5d808623d9ae03ff1f347cb730954c5
Belal Safy <81881113+Belal-Safy@users.noreply.github.com>
2023-03-27 23:36:36 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,20 +9,20 @@
   */
 void rev_string(char *s)
 {
-    int l, i;
-    char *begin_ptr, *end_ptr, ch;
+int l, i;
+char *begin_ptr, *end_ptr, ch;
 
-    l = strlen(s);
+l = strlen(s);
 
-    begin_ptr = s;
-    end_ptr = s + l - 1;
+begin_ptr = s;
+end_ptr = s + l - 1;
 
-    for (i = 0; i < (l - 1) / 2; i++) {
-        ch = *end_ptr;
-        *end_ptr = *begin_ptr;
-        *begin_ptr = ch;
+for (i = 0; i <= (l - 1) / 2; i++) {
+ch = *end_ptr;
+*end_ptr = *begin_ptr;
+*begin_ptr = ch;
 
-        begin_ptr++;
-        end_ptr--;
-    }
+begin_ptr++;
+end_ptr--;
+}
 }
